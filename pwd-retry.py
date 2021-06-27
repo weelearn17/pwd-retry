dded89c9eaf6a845c45ae25e92326726014a476f
Wenyi Chien <erin1117.cts04@g2.nctu.edu.tw>
2021-06-27 20:57:21 +0800

    correct ver

--- pwd-retry.py
+++ pwd-retry.py
@@ -7,10 +7,12 @@
 password = 'a1234'
 i = 3 # 剩餘機會
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼:')
 	if pwd == password:
-		print('登入成功！') #逃出迴圈
+		print('登入成功！')
+		break #逃出迴圈
 	else:
-		i = i - 1
-		print('密碼錯誤！還有', i,'次機會') 
-		
+		print('密碼錯誤！')
+		if i > 0:
+			print('還有', i,'次機會')
